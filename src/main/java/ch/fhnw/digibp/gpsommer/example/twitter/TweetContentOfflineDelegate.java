92932578ddbda147ac8f0d24e1f3cd8c1dad6253
Frmurillo <francis.murillo@students.fhnw.ch>
2017-05-22 16:32:28 +0200

    bugfix DMN Testversion

--- src/main/java/ch/fhnw/digibp/gpsommer/example/twitter/TweetContentOfflineDelegate.java
+++ src/main/java/ch/fhnw/digibp/gpsommer/example/twitter/TweetContentOfflineDelegate.java
@@ -10,9 +10,13 @@ public class TweetContentOfflineDelegate implements JavaDelegate {
 
   public void execute(DelegateExecution execution) throws Exception {
     String content = (String) execution.getVariable("requesterName");
+    execution.setVariable("age",20);
+    execution.setVariable("bmi",20);
 
     System.out.println("\n\n\n######\n\n\n");
     System.out.println("NAME IS: '" + content + "'");
+    System.out.println("AGE IS: '" + (Integer) execution.getVariable("age") + "'");
+    System.out.println("BMI IS: '" + (Integer) execution.getVariable("bmi") + "'");
     System.out.println("\n\n\n######\n\n\n");
   }
 
